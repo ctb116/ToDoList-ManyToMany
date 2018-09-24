e19cf95184517b88297b668104597da436792d13
Catherine Bradley and Vera Weikel <vera@>
2018-09-24 16:07:25 -0700

    add many to mant relationship between items and categories

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -1,42 +1,50 @@
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-// using System;
-// using System.Collections.Generic;
-//
-// namespace ToDoList.Controllers
-// {
-//   public class CategoriesController : Controller
-//   {
-//     [HttpGet("/categories")]
-//     public ActionResult Index()
-//     {
-//         List<Category> allCategories = Category.GetAll();
-//         return View(allCategories);
-//     }
-//
-//     [HttpGet("/categories/new")]
-//     public ActionResult CreateForm()
-//     {
-//         return View();
-//     }
-//
-//     [HttpPost("/categories")]
-//     public ActionResult Create()
-//     {
-//         Category newCategory = new Category(Request.Form["categoryName"]);
-//         newCategory.Save();
-//         return RedirectToAction("Index");
-//     }
-//
-//     [HttpGet("/categories/{id}")]
-//     public ActionResult Details(int id)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category selectedCategory = Category.Find(id);
-//       List<Item> categoryItems = selectedCategory.GetItems();
-//       model.Add("category",selectedCategory);
-//       model.Add("items",categoryItems);
-//       return View(selectedCategory);
-//     }
-//   }
-// }
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    [HttpGet("/categories")]
+    public ActionResult Index()
+    {
+      List<Category> allCategories = Category.GetAll();
+      return View(allCategories);
+    }
+
+    [HttpGet("/categories/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    [HttpPost("/categories")]
+    public ActionResult Create(string categoryName)
+    {
+      Category newCategory = new Category(categoryName);
+      newCategory.Save();
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/categories/{id}")]
+    public ActionResult Details(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category selectedCategory = Category.Find(id);
+      List<Item> categoryItems = selectedCategory.GetItems();
+      List<Item> allItems = Item.GetAll();
+      model.Add("selectedCategory", selectedCategory);
+      model.Add("categoryItems", categoryItems);
+      model.Add("allItems", allItems);
+      return View(model);
+    }
+    [HttpPost("/categories/{categoryId}/items/new")]
+    public ActionResult AddItem(int categoryId, int itemId)
+    {
+      Category category = Category.Find(categoryId);
+      Item item = Item.Find(itemId);
+      category.AddItem(item);
+      return RedirectToAction("Details",  new { id = categoryId });
+    }
+  }
+}
--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,85 +1,98 @@
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-// using System;
-// using System.Collections.Generic;
-//
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
-//     [HttpGet("/items")]
-//     public ActionResult Index()
-//     {
-//         List<Item> allItems = Item.GetAll();
-//         return View(allItems);
-//     }
-//
-//     [HttpGet("/items/new")]
-//     public ActionResult CreateForm()
-//     {
-//         List<Category> allCategories = Category.GetAll();
-//         return View(allCategories);
-//     }
-//     [HttpPost("/items")]
-//     public ActionResult Create()
-//     {
-//       Item newItem = new Item(Request.Form["itemDescription"], Request.Form["itemDueDate"], int.Parse(Request.Form["category"]));
-//       newItem.Save();
-//       List<Item> allItems = Item.GetAll();
-//       return RedirectToAction("Index");
-//     }
-//     [HttpPost("/items/sorted")]
-//     public ActionResult Filter()
-//     {
-//       List<Item> allItems = Item.Filter(Request.Form["order"]);
-//       return View("Index", allItems);
-//     }
-//     [HttpGet("/items/{id}")]
-//     public ActionResult Detail(int id)
-//     {
-//       Item newItem = Item.Find(id);
-//       return View(newItem);
-//     }
-//
-//     [HttpGet("/items/{id}/update")]
-//     public ActionResult UpdateForm(int id)
-//     {
-//         // Dictionary<List<Category>, Item> newDictionary = new Dictionary<List<Category>, Item>() {};
-//         // Item thisItem = Item.Find(id);
-//         // List<Category> allCategories = Category.GetAll();
-//         // newDictionary.Add(allCategories, thisItem);
-//         Dictionary<string, object> newDictionary = new Dictionary<string, object>();
-//         List<Category> allCategories = Category.GetAll();
-//         Item foundItem = Item.Find(id);
-//         newDictionary.Add("categories",allCategories);
-//         newDictionary.Add("item",foundItem);
-//         return View(newDictionary);
-//     }
-//
-//     [HttpPost("/items/{id}/update")]
-//     public ActionResult Update(int id, string newDescription)
-//     {
-//         Item thisItem = Item.Find(id);
-//
-//         thisItem.Edit(Request.Form["newDescription"];
-//         return RedirectToAction("Index");
-//     }
-//
-//     [HttpPost("/items/delete")]
-//     public ActionResult DeleteAll()
-//     {
-//         Item.DeleteAll();
-//         return View();
-//     }
-//
-//     [HttpGet("/items/{id}/delete")]
-//     public ActionResult Delete(int id)
-//     {
-//         Item thisItem = Item.Find(id);
-//         thisItem.Delete(id);
-//         return RedirectToAction("Index");
-//     }
-//
-//
-//   }
-// }
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string itemDescription, string itemDueDate) //userinput
+    {
+      Item newItem = new Item(itemDescription, itemDueDate);
+      newItem.Save();
+      return RedirectToAction("Index");
+    }
+    //     [HttpPost("/items/sorted")]
+    //     public ActionResult Filter()
+    //     {
+    //       List<Item> allItems = Item.Filter(Request.Form["order"]);
+    //       return View("Index", allItems);
+    // }
+    [HttpGet("/items/{id}")]
+    public ActionResult Details(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Item selectedItem = Item.Find(id);
+      List<Category> itemCategories = selectedItem.GetCategories();
+      List<Category> allCategories = Category.GetAll();
+      model.Add("selectedItem", selectedItem);
+      model.Add("itemCategories", itemCategories);
+      model.Add("allCategories", allCategories);
+      return View(model);
+    }
+    [HttpPost("/items/{itemId}/categories/new")]
+    public ActionResult AddCategory(int itemId, int categoryId)
+    {
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      item.AddCategory(category);
+      return RedirectToAction("Details",  new { id = itemId });
+    }
+    //
+    //     [HttpGet("/items/{id}/update")]
+    //     public ActionResult UpdateForm(int id)
+    //     {
+    //         // Dictionary<List<Category>, Item> newDictionary = new Dictionary<List<Category>, Item>() {};
+    //         // Item thisItem = Item.Find(id);
+    //         // List<Category> allCategories = Category.GetAll();
+    //         // newDictionary.Add(allCategories, thisItem);
+    //         Dictionary<string, object> newDictionary = new Dictionary<string, object>();
+    //         List<Category> allCategories = Category.GetAll();
+    //         Item foundItem = Item.Find(id);
+    //         newDictionary.Add("categories",allCategories);
+    //         newDictionary.Add("item",foundItem);
+    //         return View(newDictionary);
+    //     }
+    //
+    //     [HttpPost("/items/{id}/update")]
+    //     public ActionResult Update(int id, string newDescription)
+    //     {
+    //         Item thisItem = Item.Find(id);
+    //
+    //         thisItem.Edit(Request.Form["newDescription"];
+    //         return RedirectToAction("Index");
+    //     }
+    //
+    //     [HttpPost("/items/delete")]
+    //     public ActionResult DeleteAll()
+    //     {
+    //         Item.DeleteAll();
+    //         return View();
+    //     }
+    //
+    //     [HttpGet("/items/{id}/delete")]
+    //     public ActionResult Delete(int id)
+    //     {
+    //         Item thisItem = Item.Find(id);
+    //         thisItem.Delete(id);
+    //         return RedirectToAction("Index");
+    //     }
+    //
+    //
+  }
+}
